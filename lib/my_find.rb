c86f5b00f3911d34d8d8c407664364f46c6d4b14
Luis Alberto Servin <lservin2015@gmail.com>
2020-02-03 16:07:44 +0000

    Automatically backed up by Learn

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -2,7 +2,7 @@ require 'pry'
 
 def my_find(collection)
    i = 0 
-   while i < collection.length 
+   while i << collection.length 
      if yield < (collection[i])
       return collection[i]
     end 
